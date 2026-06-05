59fbcf53f75302abbbc8f9105454bea9b32b1aec
Augusto Ojeda Notebook <manuojedauniversidad@gmail.com>
2026-06-04 21:25:23 -0300

    s

---
+++ server/GestionJcc/Program.cs
@@ -0,0 +1,74 @@
+/*using GestionJcc.Datos;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+//area de servicios
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>
+    (opciones => opciones.UseSqlServer("name=DefaultConnection"));
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//area de middlewares
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+app.Run();*/
+using GestionJcc.Datos;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// �rea de servicios
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    opciones => opciones.UseSqlServer("name=DefaultConnection"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
+var app = builder.Build();
+
+// Aplicar migraciones al iniciar
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
+
+// �rea de middlewares
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GestionJcc v1");
+    });
+}
+
+app.UseStaticFiles();  
+app.UseCors("AllowAll");  
+app.UseSession();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
